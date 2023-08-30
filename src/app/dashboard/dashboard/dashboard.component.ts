fe7921dff7573695e0981546b4c0fd26a5fd439e
Andrew <andrewderbak@gmail.com>
2023-08-30 13:31:01 -0500

    Adds video list and player components to dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VideoListComponent } from '../video-list/video-list.component';
+import { VideoPlayerComponent } from '../video-player/video-player.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, VideoListComponent, VideoPlayerComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
